447431ede620e05156cc6d478ab613b350092d15
Aleksey Cheusov <vle@gmx.net>
2015-07-22 00:43:22 +0300

    heri-eval: option -m was added for generating the confusion matrix

--- tests/test_heri-eval.sh
+++ tests/test_heri-eval.sh
@@ -11,6 +11,9 @@ sort "$res_dir/errors.txt" |
 cmp 'heri-eval #1 -O' \
 '#14 1 0
 #28 0 1
+#29 0 1
+#30 0 1
+#32 0 1
 '
 
 env SVM_TRAIN_CMD=true SVM_PREDICT_CMD=rulebased_predict \
@@ -19,4 +22,12 @@ awk 'NR == 14 || NR == 28' "$res_dir/results.txt" |
 cmp 'heri-eval #2 -o' \
 '1 0
 0 1
+'
+
+env SVM_TRAIN_CMD=true SVM_PREDICT_CMD=rulebased_predict \
+    heri-eval -m "$res_dir/confusion_matrix.txt" -n5 matrix.libsvm >/dev/null
+cat "$res_dir/confusion_matrix.txt" |
+cmp 'heri-eval #3 -m' \
+'4 : 0 1
+1 : 1 0
 '
