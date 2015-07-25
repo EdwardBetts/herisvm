6c0ef96187f2ba7f1a795c624a96e948bf39334c
Aleksey Cheusov <vle@gmx.net>
2015-07-26 01:55:47 +0300

    heri-stat: new option -t was added. A number of fixes for NaN. New regression tests.

--- tests/test_heri-stat.sh
+++ tests/test_heri-stat.sh
@@ -18,9 +18,9 @@ cmp 'heri-stat #1.1 defaults {golden,result}5.txt' \
 Class  B      P, R, F1:  1          2/2      ,  0.6667     2/3      ,  0.8   
 Class  C      P, R, F1:  1          3/3      ,  0.75       3/4      ,  0.8571
 Class  E      P, R, F1:  1          5/5      ,  0.8333     5/6      ,  0.9091
-Class  O      P, R, F1:  0          0/4      ,  NaN        0/0      ,  NaN   
+Class  O      P, R, F1:  0          0/4      ,  0          0/0      ,  0     
 Accuracy              :  0.7333    11/15     
-Macro average P, R, F1:  0.8                 ,  NaN                 ,  NaN   
+Macro average P, R, F1:  0.8                 ,  0.55                ,  0.6466
 '
 
 heri-stat -uO golden5.txt result5.txt 2>&1 |
@@ -122,10 +122,10 @@ Macro average	f1	0.NNNN
 heri-stat golden3.txt result3.txt 2>&1 |
 remove_fractions |
 cmp 'heri-stat #8 all equal' \
-'Class  A      P, R, F1:  NaN        0/0      ,  0          0/6      ,  NaN   
-Class  B      P, R, F1:  0          0/6      ,  NaN        0/0      ,  NaN   
+'Class  A      P, R, F1:  1          0/0      ,  0          0/6      ,  0     
+Class  B      P, R, F1:  0          0/6      ,  0          0/0      ,  0     
 Accuracy              :  0          0/6      
-Macro average P, R, F1:  NaN                 ,  NaN                 ,  NaN   
+Macro average P, R, F1:  0.NNNN                 ,  0                   ,  0     
 '
 
 heri-stat golden3.txt /dev/null 2>&1 |
@@ -157,4 +157,54 @@ Class  B      P, R, F1:  0.5        1/2      ,  0.5        1/2      ,  0.5
 Class  C      P, R, F1:  0.5        1/2      ,  1          1/1      ,  0.6667
 Accuracy              :  0.6667     4/6      
 Macro average P, R, F1:  0.6667              ,  0.7222              ,  0.6556
+'
+
+heri-stat -t0.5 golden5.txt result5_prob.txt 2>&1 |
+cmp 'heri-stat #13.1 -t 0.5' \
+'Class  A      P, R, F1:  1          2/2      ,  1          2/2      ,  1     
+Class  B      P, R, F1:  1          3/3      ,  1          3/3      ,  1     
+Class  C      P, R, F1:  1          4/4      ,  1          4/4      ,  1     
+Class  E      P, R, F1:  1          6/6      ,  1          6/6      ,  1     
+Micro average P, R, F1:  1         15/15     ,  1         15/15     ,  1     
+Macro average P, R, F1:  1                   ,  1                   ,  1     
+'
+
+heri-stat -t 0.6 golden5.txt result5_prob.txt 2>&1 |
+cmp 'heri-stat #13.2 -t 0.6' \
+'Class  A      P, R, F1:  1          2/2      ,  1          2/2      ,  1     
+Class  B      P, R, F1:  1          3/3      ,  1          3/3      ,  1     
+Class  C      P, R, F1:  1          4/4      ,  1          4/4      ,  1     
+Class  E      P, R, F1:  1          5/5      ,  0.8333     5/6      ,  0.9091
+Micro average P, R, F1:  1         14/14     ,  0.9333    14/15     ,  0.9655
+Macro average P, R, F1:  1                   ,  0.9583              ,  0.9773
+'
+
+heri-stat -t 0.7 golden5.txt result5_prob.txt 2>&1 |
+cmp 'heri-stat #13.3 -t 0.7' \
+'Class  A      P, R, F1:  1          1/1      ,  0.5        1/2      ,  0.6667
+Class  B      P, R, F1:  1          3/3      ,  1          3/3      ,  1     
+Class  C      P, R, F1:  1          4/4      ,  1          4/4      ,  1     
+Class  E      P, R, F1:  1          5/5      ,  0.8333     5/6      ,  0.9091
+Micro average P, R, F1:  1         13/13     ,  0.8667    13/15     ,  0.9286
+Macro average P, R, F1:  1                   ,  0.8333              ,  0.8939
+'
+
+heri-stat -t 0.8 golden5.txt result5_prob.txt 2>&1 |
+cmp 'heri-stat #13.4 -t 0.8' \
+'Class  A      P, R, F1:  1          1/1      ,  0.5        1/2      ,  0.6667
+Class  B      P, R, F1:  1          2/2      ,  0.6667     2/3      ,  0.8   
+Class  C      P, R, F1:  1          4/4      ,  1          4/4      ,  1     
+Class  E      P, R, F1:  1          5/5      ,  0.8333     5/6      ,  0.9091
+Micro average P, R, F1:  1         12/12     ,  0.8       12/15     ,  0.8889
+Macro average P, R, F1:  1                   ,  0.75                ,  0.8439
+'
+
+heri-stat -t 0.93 golden5.txt result5_prob.txt 2>&1 |
+cmp 'heri-stat #13.5 -t 0.93' \
+'Class  A      P, R, F1:  1          0/0      ,  0          0/2      ,  0     
+Class  B      P, R, F1:  1          0/0      ,  0          0/3      ,  0     
+Class  C      P, R, F1:  1          3/3      ,  0.75       3/4      ,  0.8571
+Class  E      P, R, F1:  1          3/3      ,  0.5        3/6      ,  0.6667
+Micro average P, R, F1:  1          6/6      ,  0.4        6/15     ,  0.5714
+Macro average P, R, F1:  1                   ,  0.3125              ,  0.381 
 '
